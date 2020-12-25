0e45a7ab0793fc1da49d5c018f5d4e32ed0abb96
M <m>
2020-12-25 18:00:40 +0900

    リンクの位置修正

--- src/components/Atoms/cards.js
+++ src/components/Atoms/cards.js
@@ -1,21 +1,29 @@
-import React from "react"
+import React, { useState, useEffect } from "react"
 import ArrowForwardIosIcon from "@material-ui/icons/ArrowForwardIos"
+import { Link } from "gatsby"
 
 import Style from "styles/project.module.scss"
 
-export default function Cards(props) {
+export default function Card(props) {
   return (
-    <div className={Style.cards}>
-      <img src={props.bgImagePath} className={Style.projectImage} />
-      <div className={Style.projectContent}>
-        <h3 className={Style.projectName}>{props.projectName}</h3>
-        <div className={Style.hoverBox}>
-          <a href="/" className={Style.projectLink}>
-            Visit Project{" "}
-            <ArrowForwardIosIcon fontSize="small" className={Style.linkIcon} />
-          </a>
+    <>
+      <Link to={props.link} className={Style.cards}>
+        <img src={props.bgImagePath} className={Style.projectImage} />
+        <div className={Style.projectContent}>
+          <h3 className={Style.projectName}>{props.projectName}</h3>
+          <div className={Style.hoverBox}>
+            <div className={Style.projectLink}>
+              <p>
+                Visit Project
+                <ArrowForwardIosIcon
+                  fontSize="small"
+                  className={Style.linkIcon}
+                />
+              </p>
+            </div>
+          </div>
         </div>
-      </div>
-    </div>
+      </Link>
+    </>
   )
 }
